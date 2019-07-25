306a9f4846afd285465b91d4914b08c780a91b01
davygallegos <david.gallegos@outlook.com>
2019-07-24 19:54:35 -0700

    getting rid of discrepancies

---
+++ client/src/components/login/google.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const google = {
+    client_id: "YOUR_CLIENT_ID.apps.googleusercontent.com",
+    scope: "profile email"
+  };
+   
+  export default  google;
+
+
+
+
+
+
+  
