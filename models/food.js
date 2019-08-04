e58d356fd0d8a1fdec5e521bfc6ec1b9dd64fb9c
HowardKuo <howardkuo91@gmail.com>
2019-08-04 14:26:14 -0700

    fixed heroku mongo db link

--- models/food.js
+++ models/food.js
@@ -2,13 +2,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const foodSchema = new Schema({
-  title: { type: String, required: true },
-  subtitle: { type: String },
-  authors: { type: [String], required: true },
-  link: { type: String, required: true },
-  description: { type: String, required: true },
-  image: { type: String, required: true },
-  wolfId: { type: String, required: true, unique: true }
+  id: { type: Number, required: true, unique: true},
+  foodName: { type: String, required: true },
+  calories: { type: Number, required: true},
+  proteins: { type: Number, required: true},
+  fats: { type: Number, required: true}
+  //title: { type: String, required: true },
+  //subtitle: { type: String },
+  //authors: { type: [String], required: true },
+  //link: { type: String, required: true },
+  //description: { type: String, required: true },
+  //image: { type: String, required: true },
+ // wolfId: { type: String, required: true, unique: true }
 });
 
 const Food = mongoose.model("Food", foodSchema);
