67628b77f788448775c3c2287848e0a9155ea6cc
HowardKuo <howardkuo91@gmail.com>
2019-08-13 22:04:38 -0700

    progressbar WIP

--- client/src/components/Progress/index.js
+++ client/src/components/Progress/index.js
@@ -1,30 +1,53 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { MDBBtn } from "mdbreact";
 import { MDBProgress } from 'mdbreact';
 import '../Progress/style.css';
+import API from '../../utils/API';
 
-const Progress = () => {
-  return (
-    <Row className="block">
-        <Col md={12}>
-            <div>
-            <h3>Progress</h3>
-            <Row>
-                <Col md={12}>
-                <MDBProgress material value={25} height="20px" color="danger" animated>
-                    25%
-                </MDBProgress>
-                </Col>
-            </Row>
-            <Row>
-                <Col md={2}><MDBBtn>Update</MDBBtn></Col><Col md={4}><p>Target:  cals.   -       =    </p></Col>
-            </Row>
-            </div>  
-        </Col>
-    </Row> 
-  );
+class Progress extends React.Component {
+state = {  
+    
+}
+
+componentDidMount() {
+    this.loadCalories();
+}
+
+loadCalories = () => {
+
+    API.getFoods(response => {
+        console.log(response)
+        const totalCalories = response.data.reduce((a,b) => a + parseInt(b.calories), 0)
+        this.setState({
+            totalCalories: totalCalories,
+            progressBar: (totalCalories/2000) * 100,
+        })
+    })
+}
+
+
+render () {
+    return (
+        <Row className="block">
+            <Col md={12}>
+                <div>
+                <h3>Progress</h3>
+                <Row>
+                    <Col md={12}>
+                    <MDBProgress material value={this.state.progressBar} height="20px" color="danger" animated>
+                        {this.state.totalCalories}
+                    </MDBProgress>
+                    </Col>
+                </Row>
+                <Row>
+
+                </Row>
+                </div>  
+            </Col>
+        </Row> 
+    );
+}
 }
 
 export default Progress;
